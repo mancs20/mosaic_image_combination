0182d1d23b52b740683d8eea2746475e188f5d68
Manuel Combarro Simon <>
2022-08-01 18:38:11 +0200

    Colors for 20 pictures
    This need to be done, is not ready yet

---
+++ constants.py
@@ -0,0 +1,79 @@
+# Colors for 20 pictures
+
+#d3d3d3
+
+# maroon2
+#
+# #7f0000
+#
+# olive
+#
+# #808000
+#
+# mediumseagreen
+#
+# #3cb371
+#
+# darkcyan
+#
+# #008b8b
+#
+# darkmagenta
+#
+# #8b008b
+#
+# red
+#
+# #ff0000
+#
+# darkorange
+#
+# #ff8c00
+#
+# gold
+#
+# #ffd700
+#
+# mediumblue
+#
+# #0000cd
+#
+# springgreen
+#
+# #00ff7f
+#
+# royalblue
+#
+# #4169e1
+#
+# darksalmon
+#
+# #e9967a
+#
+# aqua
+#
+# #00ffff
+#
+# deepskyblue
+#
+# #00bfff
+#
+# greenyellow
+#
+# #adff2f
+#
+# fuchsia
+#
+# #ff00ff
+#
+# khaki
+#
+# #f0e68c
+#
+# plum
+#
+# #dda0dd
+#
+# deeppink
+#
+# #ff1493
