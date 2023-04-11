6360ceb61237f798f7647e681d683bf04fb128ef
Manuel Combarro Simon <>
2023-04-11 17:42:03 +0200

    Find all polygons resulting from the intersections of the images and form a set
    Each image now is a set, where the elements are the resulting polygon that are totally included inside it

--- Strategies/StrategyDiscrete/CPWithouyClouds.py
+++ Strategies/StrategyDiscrete/CPWithouyClouds.py
@@ -7,6 +7,29 @@ class CPWithoutClouds(StrategyDiscrete):
     number_of_runs = 1
     def run_strategy(self):
         super().discretize()
-        self.call_cp(self.sets_images, self.universe)
-    def call_cp(self):
+        self.export_set_images_universe_to_dzn(self.sets_images, self.universe)
+        # self.call_cp_solver(self.sets_images, self.universe)
+
+    def export_set_images_universe_to_dzn(self, sets_images, universe):
+        with open(self.path + "/CPWithoutClouds.dzn", "w") as f:
+            f.write("images = " + str(len(sets_images)) + ";\n")
+            f.write("universe = " + str(universe) + ";\n")
+            f.write("sets = [")
+            for i in range(len(sets_images)):
+                f.write("{")
+                for j in range(len(sets_images[i].list_of_regions)):
+                    f.write(str(sets_images[i].list_of_regions[j]))
+                    if j < len(sets_images[i].list_of_regions) - 1:
+                        f.write(", ")
+                f.write("}")
+                if i < len(sets_images) - 1:
+                    f.write(", ")
+            f.write("];\n")
+            f.write("costs = [")
+            for i in range(len(sets_images)):
+                f.write(str(sets_images[i].weight))
+                if i < len(sets_images) - 1:
+                    f.write(", ")
+            f.write("];\n")
+    def call_cp_solver(self):
         result = 1;
--- Strategies/StrategyDiscrete/StrategyDiscrete.py
+++ Strategies/StrategyDiscrete/StrategyDiscrete.py
@@ -1,9 +1,10 @@
-import copy
-
-from geopandas import GeoDataFrame
+from shapely.geometry import LineString
+from shapely.ops import unary_union, polygonize
 
 from Strategies.Strategy import Strategy
 from abc import abstractmethod
+
+from Strategies.StrategyDiscrete.ImageSet import ImageSet
 
 
 class StrategyDiscrete(Strategy):
@@ -17,9 +18,51 @@ class StrategyDiscrete(Strategy):
     def run_strategy(self):
         pass
 
-    def discretize(self):
+    def discretize(self, method="getAllPolygonsWithShapely"):
         self.remove_image_area_outside_aoi()
-        self.sets_images = self.discretize_brute_force()
+        self.initialize_set_images()
+        if method == "getAllPolygonsWithShapely":
+            self.get_intersections_with_shapely()
+
+    def initialize_set_images(self):
+        for i in range(len(self.contained_images)):
+            image_set = ImageSet(image_id=self.contained_images.loc[i, 'image_id'],
+                                 weight=self.contained_images.loc[i, 'area'], list_of_regions=[])
+            self.sets_images.append(image_set)
+
+    def get_intersections_with_shapely(self):
+        polygons = self.contained_images.geometry
+        rings = [LineString(list(pol.exterior.coords)) for pol in polygons]
+        union = unary_union(rings)
+        resulting_polygons = [geom for geom in polygonize(union)]
+        self.universe = len(resulting_polygons)
+        self.associate_resulting_polygons_to_images(resulting_polygons)
+
+    def associate_resulting_polygons_to_images(self, resulting_polygons):
+        for i in range(len(self.sets_images)):
+            for j in range(len(resulting_polygons)):
+                polygon_centroid = resulting_polygons[j].centroid
+                if self.contained_images.loc[i, 'geometry'].contains(polygon_centroid):
+                    self.sets_images[i].list_of_regions.append(j + 1)
+
+    @staticmethod
+    def plot_polygons(polygons):
+        from matplotlib import pyplot as plt
+
+        fig, ax = plt.subplots()
+        # for linestring in linestrings:
+        #     # extract the x and y coordinates of the line segments
+        #     x, y = linestring.xy
+        #
+        #     # plot the line segments
+        #     ax.plot(x, y, color='red', linewidth=2, solid_capstyle='round')
+
+        for polygon in polygons:
+            x, y = polygon.exterior.xy
+            ax.fill(x, y, alpha=0.5, edgecolor="black")
+            centroid = polygon.centroid
+            ax.scatter(centroid.x, centroid.y, color='red')
+        plt.show()
 
     def remove_image_area_outside_aoi(self):
         intersection_images_aoi = self.images.intersection(self.aoi.unary_union)
