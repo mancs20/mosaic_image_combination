cd9e4a8ad5da22afbec37abbbb09f4d7b4d3711d
Manuel Combarro Simon <>
2023-04-11 17:37:00 +0200

    Modified clases ImageSet and Regions for Discrete approaches

--- Strategies/StrategyDiscrete/ImageSet.py
+++ Strategies/StrategyDiscrete/ImageSet.py
@@ -1,5 +1,5 @@
 class ImageSet:
-    def __int__(self):
-        self.image_id = 0
-        self.list_of_regions = []
-        self.weight = 0
+    def __init__(self, image_id, weight, list_of_regions):
+        self.image_id = image_id
+        self.weight = weight
+        self.list_of_regions = list_of_regions
